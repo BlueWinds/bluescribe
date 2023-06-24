52e47e014474eb8aa396a46bc0f9a999892d0f0b
BlueWinds <blue@everblue.info>
2023-06-24 07:56:10 -0700

    Pull profile order from gameSystem rather than hardcoded list

--- src/Force/Profiles.js
+++ src/Force/Profiles.js
@@ -1,43 +1,42 @@
 import _ from 'lodash'
 
 import { findId, getMinCount } from '../utils'
-
-const order = {
-  Unit: 1,
-  Weapon: 2,
-  Abilities: 3,
-}
+import { useSystem } from '../Context'
 
 const Profiles = ({ profiles, number }) => {
+  const gameData = useSystem()
+
   return (
     <>
-      {_.sortBy(Object.keys(profiles), (p) => order[p] || p).map((type) => (
-        <table className="profile" key={type}>
-          <thead>
-            <tr>
-              <th>{type}</th>
-              {profiles[type][0][1].characteristics.characteristic.map((c) => (
-                <th key={c.name}>{c.name}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {_.sortBy(profiles[type], '1.name').map(([number, profile]) => (
-              <tr key={profile.id}>
-                <td>
-                  {number > 1 ? `x${number} ` : ''}
-                  {profile.name}
-                </td>
-                {profile.characteristics.characteristic.map((c) => (
-                  <td className="profile" key={c.name}>
-                    {c['#text']}
-                  </td>
+      {gameData.gameSystem.profileTypes?.map(({ name, characteristicTypes }) =>
+        profiles[name] ? (
+          <table className="profile" key={name}>
+            <thead>
+              <tr>
+                <th>{name}</th>
+                {characteristicTypes.map((ct) => (
+                  <th key={ct.name}>{ct.name}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ))}
+            </thead>
+            <tbody>
+              {_.sortBy(profiles[name], '1.name').map(([number, profile]) => (
+                <tr key={profile.id}>
+                  <td>
+                    {number > 1 ? `x${number} ` : ''}
+                    {profile.name}
+                  </td>
+                  {profile.characteristics.characteristic.map((c) => (
+                    <td className="profile" key={c.name}>
+                      {c['#text']}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : null,
+      )}
     </>
   )
 }
